f3ce12e0f3d024ecf1f714bcbfb5273c92321368
OisinKeaneOMahony <20084833@mail.wit.ie>
2024-08-24 22:14:45 +0100

    Mongo lab Exercises done

--- tasky-api/index.js
+++ tasky-api/index.js
@@ -3,6 +3,7 @@ import express from 'express';
 import tasksRouter from './api/tasks';
 import './db';
 import usersRouter from './api/users';
+import cors from 'cors';
 
 dotenv.config();
 
@@ -19,9 +20,10 @@ const app = express();
 const port = process.env.PORT;
 
 app.use(express.json());
+app.use(cors());
 app.use('/api/tasks', tasksRouter);
-app.use(errHandler);
 app.use('/api/users', usersRouter);
+app.use(errHandler);
 
 app.listen(port, () => {
   console.info(`Server running at ${port}`);
