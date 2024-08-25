924f19bdabe7785da3c1d8ead9b241c58df967c0
OisinKeaneOMahony <20084833@mail.wit.ie>
2024-08-25 03:23:38 +0100

    Salt and Hash

--- movies-api/api/users/userModel.js
+++ movies-api/api/users/userModel.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const Schema = mongoose.Schema;
 
@@ -12,6 +13,30 @@ const passwordValidator = {
 const UserSchema = new Schema({
     username: { type: String, unique: true, required: true },
     password: { type: String, required: true, validate: passwordValidator }
+});
+
+UserSchema.statics.findByUsername = function (username) {
+    return this.findOne({ username: username });
+};
+
+UserSchema.methods.comparePassword = async function (passw) {
+    return await bcrypt.compare(passw, this.password);
+};
+
+UserSchema.pre('save', async function(next) {
+    const saltRounds = 10; // you can change the value
+    // const user = this;
+    if (this.isModified('password') || this.isNew) {
+        try {
+            const hash = await bcrypt.hash(this.password, saltRounds);
+            this.password = hash;
+            next();
+        } catch (error) {
+            next(error);
+        }
+    } else {
+        next();
+    }
 });
 
 export default mongoose.model('User', UserSchema);
